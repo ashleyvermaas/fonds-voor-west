0b699a49ef085497962e67e3ef8b4a0fc9d2e30f
Ashley Vermaas <ashleyvermaas1@gmail.com>
2020-12-10 14:17:00 +0100

    create functionality delete and edit projects, minor bug with showing date

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -36,6 +36,24 @@ router.post('/projects/:id/delete', (req, res, next) => {
 });
 
 // Routes to edit a project
+router.get('/projects/:id/edit', (req, res, next) => {
+  const { id } = req.params;
+  
+  Project.findById(id)
+  .then((projectFromDB) => {
+    console.log(projectFromDB); 
+    res.render('projects/edit', projectFromDB);})
+  .catch((error) => next(error));
+});
+
+router.post('/projects/:id/edit', (req, res, next) => {
+  const { id } = req.params;
+  const { name, date, location, description } = req.body;
+
+  Project.findByIdAndUpdate(id, req.body, {new: true})
+  .then(() => res.redirect('/projects'))
+  .catch((error) => next(error));
+});
 
 module.exports = router;
 

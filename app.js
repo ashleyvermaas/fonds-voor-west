dac603c6158372367e78106d28c2a4882c91ad90
Dirk Kelderman <dirkkelderman@gmail.com>
2020-12-27 13:50:24 +0100

    profile picture and project image size now restricted

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ function(accessToken, refreshToken, profile, done) {
 passport.use(new LinkedInStrategy({
   clientID: process.env.LINKEDIN_KEY,
   clientSecret: process.env.LINKEDIN_SECRET,
-  callbackURL: "http://localhost:3000/auth/linkedin/callback",
+  callbackURL: "/auth/linkedin/callback",
   scope: ['r_emailaddress', 'r_liteprofile'],
   state: true
 }, function(accessToken, refreshToken, profile, done) {

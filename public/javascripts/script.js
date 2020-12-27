608334b001e44bfeba5aa369eb2bef05abde4b1c
Dirk Kelderman <dirkkelderman@gmail.com>
2020-12-27 16:18:52 +0100

    auto fill-in for adress, tried some google maps again

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,15 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
 }, false);
 
+
 const amsterdamWest = {
   lat: 52.37524007976412, 
   lng: 4.862769947828028 
 }
 
-
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 13,
+    zoom: 14,
     center: amsterdamWest,
   });
   const geocoder = new google.maps.Geocoder();
@@ -22,11 +22,10 @@ function initMap() {
 }
 
 function geocodeAddress(geocoder, resultsMap) {
-  const address = document.getElementById("location").value;
+  const address = document.getElementById("address").value;
   geocoder.geocode({ address: address }, (results, status) => {
     if (status === "OK") {
       resultsMap.setCenter(results[0].geometry.location);
-      console.log(results[0].geometry.location)
       new google.maps.Marker({
         map: resultsMap,
         position: results[0].geometry.location,
@@ -54,3 +53,58 @@ function projectStatus(){
 }
 
 projectStatus();
+
+let placeSearch;
+let autocomplete;
+const componentForm = {
+  street_number: "short_name",
+  route: "long_name",
+  locality: "long_name",
+  administrative_area_level_1: "short_name",
+  country: "long_name",
+  postal_code: "short_name",
+};
+
+function initAutocomplete() {
+  autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById("autocomplete"),
+    { types: ["geocode"] }
+  );
+
+  autocomplete.setFields(["address_component"]);
+  autocomplete.addListener("place_changed", fillInAddress);
+}
+
+function fillInAddress() {
+  const place = autocomplete.getPlace();
+
+  for (const component in componentForm) {
+    document.getElementById(component).value = "";
+    document.getElementById(component).disabled = false;
+  }
+
+  for (const component of place.address_components) {
+    const addressType = component.types[0];
+
+    if (componentForm[addressType]) {
+      const val = component[componentForm[addressType]];
+      document.getElementById(addressType).value = val;
+    }
+  }
+}
+
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const geolocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+      const circle = new google.maps.Circle({
+        center: geolocation,
+        radius: position.coords.accuracy,
+      });
+      autocomplete.setBounds(circle.getBounds());
+    });
+  }
+}

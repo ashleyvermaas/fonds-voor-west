9d11d7758f25b7a4b0c3572e63d189a4dc93a57c
Dirk Kelderman <dirkkelderman@gmail.com>
2021-01-07 18:45:56 +0100

    reset password works, ohh yeahh

--- models/User.model.js
+++ models/User.model.js
@@ -127,29 +127,29 @@ const userSchema = new Schema({
     facebookId: String,
   });
 
-  userSchema.pre('save', function(next) {
-    var user = this;
-    var SALT_FACTOR = 5;
+  // userSchema.pre('save', function(next) {
+  //   var user = this;
+  //   var SALT_FACTOR = 5;
   
-    if (!user.isModified('password')) return next();
+  //   if (!user.isModified('password')) return next();
   
-    bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
-      if (err) return next(err);
+  //   bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+  //     if (err) return next(err);
   
-      bcrypt.hash(user.password, salt, null, function(err, hash) {
-        if (err) return next(err);
-        user.password = hash;
-        next();
-      });
-    });
-  });
+  //     bcrypt.hash(user.password, salt, null, function(err, hash) {
+  //       if (err) return next(err);
+  //       user.password = hash;
+  //       next();
+  //     });
+  //   });
+  // });
   
-  userSchema.methods.comparePassword = function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-      if (err) return cb(err);
-      cb(null, isMatch);
-    });
-  };
+  // userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  //   bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+  //     if (err) return cb(err);
+  //     cb(null, isMatch);
+  //   });
+  // };
 
 module.exports = model('User', userSchema);
 
